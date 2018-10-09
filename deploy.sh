b3a1efb5f1436c4c858071844478ebe235b38cc0
Federico Martinez <fed.x.gibson@gmail.com>
2018-10-09 19:45:39 -0300

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 if [ -z "$1" ]
+  git add -A
+  git commit -m "$1"
+  git push origin master:src
+  git subtree push --prefix dist origin master
 then
   echo Commit message not provided.
   exit
 fi
-git add -A
-git commit -m "$1"
-git push origin master:src
-git subtree push --prefix dist origin master
