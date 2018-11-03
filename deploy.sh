0aaa8d460681477436066def230d1484e3a12674
Federico Martinez <fed.x.gibson@gmail.com>
2018-11-03 16:06:15 -0300

    Change Links

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 if [ -z "$1" ]
+then
+  echo Commit message not provided.
+  exit
+else
   git add -A
   git commit -m "$1"
   git push origin master:src
   git subtree push --prefix dist origin master
-then
-  echo Commit message not provided.
-  exit
 fi
